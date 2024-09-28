97b02e868fd5032f7281b537f3571b5238123075
Samay Desai <samaydesai@gmail.com>
2024-09-28 18:41:22 -0400

    build initial ui for dashboard and form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,48 @@
+'use client'
+import { Link } from '@chakra-ui/next-js'
 import Image from "next/image";
 import { InitApiCall, ProcessImageRequest, TestApiCall, TestProcessImageRequest } from "./ocr/api";
+import Navigation from '@/components/navbar';
+import EventForm from '@/components/event-form';
 
 export default async function Home() {
 
   // let openai = await InitApiCall()
   // let test_call = await TestApiCall(openai)
   // let info = await ProcessImageRequest(openai, `./public/examples/Image from i.pinimg.com.jpg`);
-  let events = await TestProcessImageRequest()
+  // let events = await TestProcessImageRequest()
 
   return (
+    // <div>
+    //   <h1>HackCal!</h1>
+
+    //   {
+    //     events.map((event, i) => {
+    //       return (
+    //         <div key={i}>
+    //           <h2>{event.name}</h2>
+    //           <p>&emsp;Start: {event.start.format("dddd, MMMM D, YYYY h:mm A")}</p>
+    //           <p>&emsp;End: {event.end.format("dddd, MMMM D, YYYY h:mm A")}</p>
+    //           <p>&emsp;Location: {event.location}</p>
+    //           <p>&emsp;Description: {event.description}</p>
+    //         </div>
+    //       )
+    //     })
+    //   }
+
+    // </div>
     <div>
-      <h1>HackCal!</h1>
-
-      {
-        events.map((event, i) => {
-          return (
-            <div key={i}>
-              <h2>{event.name}</h2>
-              <p>&emsp;Start: {event.start.format("dddd, MMMM D, YYYY h:mm A")}</p>
-              <p>&emsp;End: {event.end.format("dddd, MMMM D, YYYY h:mm A")}</p>
-              <p>&emsp;Location: {event.location}</p>
-              <p>&emsp;Description: {event.description}</p>
-            </div>
-          )
-        })
-      }
-
+    <Navigation>
+      
+       
+    </Navigation>
+    <div>
+      <EventForm> 
+        
+        </EventForm>
+      </div>
     </div>
+    
+    
   );
 }
