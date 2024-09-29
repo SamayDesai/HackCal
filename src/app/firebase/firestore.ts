888d739375311434fffa06bff7803fd62f1f974e
VigneshSK17 <vigsk17@gmail.com>
2024-09-29 00:08:16 -0400

    Added method to read from db in a continuous manner

--- src/app/firebase/firestore.ts
+++ src/app/firebase/firestore.ts
@@ -1,6 +1,6 @@
 import { FirebaseOptions } from "firebase/app";
 import firebase from "firebase/compat/app";
-import { addDoc, collection, Firestore, getFirestore, onSnapshot, query, where } from "firebase/firestore";
+import { addDoc, collection, Firestore, getDoc, getDocs, getFirestore, initializeFirestore, onSnapshot, orderBy, query, where } from "firebase/firestore";
 import dayjs, { Dayjs } from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from 'dayjs/plugin/utc'
@@ -9,20 +9,26 @@ import { Event } from "../models"
 
 export function InitFirestore() {
 
+    console.log(process.env["NEXT_PUBLIC_FIREBASE_PROJECT_ID"])
+    console.log(process.env["NEXT_PUBLIC_FIREBASE_API_KEY"])
+
     const firebaseConfig: FirebaseOptions  = {
-        apiKey: process.env["FIREBASE_API_KEY"],
-        authDomain: process.env["FIREBASE_AUTH_DOMAIN"],
-        projectId: process.env["FIREBASE_PROJECT_ID"],
-        storageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
-        messagingSenderId: process.env["FIREBASE_MESSAGING_SENDER_ID"],
-        appId: process.env["FIREBASE_APP_ID"],
-        measurementId: process.env["FIREBASE_MEASUREMENT_ID"]
+        apiKey: process.env["NEXT_PUBLIC_FIREBASE_API_KEY"],
+        authDomain: process.env["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"],
+        projectId: process.env["NEXT_PUBLIC_FIREBASE_PROJECT_ID"],
+        storageBucket: process.env["NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"],
+        messagingSenderId: process.env["NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"],
+        appId: process.env["NEXT_PUBLIC_FIREBASE_APP_ID"],
+        measurementId: process.env["NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID"]
     }
 
     const app = firebase.initializeApp(firebaseConfig);
 
     // initialize firestore
-    return getFirestore(app)
+    return initializeFirestore(app, {
+        experimentalForceLongPolling: true,
+    })
+    // return getFirestore(app, "(default)")
 
 }
 
@@ -43,16 +49,14 @@ export function WriteNewEvent(db: Firestore, user_id: string, event: Event) {
 }
 
 export function GetAllForUser(db: Firestore, user_id: string, setEvents: (events: Event[]) => void) {
-    onSnapshot(collection(db, `users/${user_id}/events/`), (snapshot) => {
+    onSnapshot(query(collection(db, `users/${user_id}/events`), orderBy("createdAt", "desc")), (snapshot) => {
         const events: Event[] = []
-        const eventsSnapshot = snapshot.docs.sort((a, b) => {
-            return dayjs(b.get("createdAt")).unix() - dayjs(a.get("createdAt")).unix()
-        })
-        for (const event of eventsSnapshot) {
+        for (const event of snapshot.docs) {
             events.push(event.data() as Event)
         }
+        console.log(events)
         setEvents(events)
-    })
+    }, (error) => console.log(error.cause))
 
 
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Navigation from '@/components/navbar';
 import EventForm from '@/components/event-form';
 import { GetAllForUser, InitFirestore, WriteNewEvent } from "./firebase/firestore";
 import { Firestore } from "firebase/firestore";
+import dayjs from "dayjs";
 
 // TODO: Make backend API call to run OpenAI
 
@@ -23,28 +24,33 @@ export default function Home() {
 
   useEffect(() => {
     setDb(InitFirestore())
-    while (!db) {}
-    GetAllForUser(db as Firestore, "1", setEvents)
-    console.log("Events", events)
 
     // const getEvents = async () => {
-    // let openai = await InitOpenAi()
-    // let test_call = await TestApiCall(openai)
-    // let info = await ProcessImageRequest(openai, `./public/examples/Image from i.pinimg.com.jpg`);
-    // let events = await TestProcessImageRequest()
+    // // let openai = await InitOpenAi()
+    // // let test_call = await TestApiCall(openai)
+    // // let info = await ProcessImageRequest(openai, `./public/examples/Image from i.pinimg.com.jpg`);
+    // // let events = await TestProcessImageRequest()
 
-      // const eventsJson = await TestProcessImageRequest()
-      // const events = parseEvents(eventsJson)
-      // setEvents(events)
+    //   // const eventsJson = await TestProcessImageRequest()
+    //   // const events = parseEvents(eventsJson)
+    //   // setEvents(events)
 
-      // for (let event of events) {
-      //   await WriteNewEvent("1", event)
-      // }
+    //   // for (let event of events) {
+    //   //   await WriteNewEvent("1", event)
+    //   // }
+    //   await TestGetAllForUser(db as Firestore, "1", setEvents)
 
     // }
     // getEvents()
 
   }, []) // TODO: Remove empty array param
+
+  useEffect(() => {
+    console.log(db)
+    if (db) {
+      GetAllForUser(db as Firestore, "1", setEvents)
+    }
+  }, [db])
 
   return (
     // <div>
@@ -68,8 +74,8 @@ export default function Home() {
             return (
               <div key={i}>
                 <h2>{event.name}</h2>
-                <p>&emsp;Start: {event.start.format("dddd, MMMM D, YYYY h:mm A")}</p>
-                <p>&emsp;End: {event.end.format("dddd, MMMM D, YYYY h:mm A")}</p>
+                <p>&emsp;Start: {dayjs(event.start).format("dddd, MMMM D, YYYY h:mm A")}</p>
+                <p>&emsp;End: {dayjs(event.end).format("dddd, MMMM D, YYYY h:mm A")}</p>
                 <p>&emsp;Location: {event.location}</p>
                 <p>&emsp;Description: {event.description}</p>
               </div>
