80112437079df0f0699f228f03b933e541f0c2e2
VigneshSK17 <vigsk17@gmail.com>
2024-10-06 16:32:40 -0400

    One last fix to make button work necessary

--- src/app/openai.ts
+++ src/app/openai.ts
@@ -4,12 +4,6 @@ import OpenAI from "openai"
 import { promises as fs } from 'fs';
 import { Event, parseEvents } from "./models";
 
-export async function ProcessImage(file: string, events: Event[], setEvents: (events: Event[]) => void) {
-    let openai = await InitOpenAi()
-    let newEvents = await ProcessImageRequest(openai, file)
-    setEvents(newEvents.concat(events))
-}
-
 export async function InitOpenAi() {
     return new OpenAI({
         apiKey: process.env["OPENAI_API_KEY"]
